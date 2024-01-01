71e046c8f1639959e24c26706388c97627dd8ea6
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-01 19:54:49 +0100

    pokemonSlice

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
+import { PokemonApp } from './PokemonApp.jsx'
 
 import { store } from './store'
 
@@ -12,7 +13,8 @@ import { store } from './store'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 )
