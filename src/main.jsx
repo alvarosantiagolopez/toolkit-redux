95bbdb2847d1c48110ea3b24fe3abed0e14caa57
alvarosantiagolopez <alvarosantiagolopez97@gmail.com>
2024-01-03 18:47:18 +0100

    rtk query

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux'
 import { PokemonApp } from './PokemonApp.jsx'
 
 import { store } from './store'
+import { TodoApp } from './TodoApp.jsx'
 
 
 
@@ -14,7 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 )
